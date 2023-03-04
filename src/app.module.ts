5833b218ec58542e7c270ae77972d1d10914afad
Sensui <sensuijunior@hotmail.com>
2023-03-04 11:57:25 -0300

    feature: server config settled

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [],
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { serverConfig } from './config/server.config';
 
 async function bootstrap() {
+  const appServerConfig = serverConfig();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(appServerConfig.port);
 }
 bootstrap();
