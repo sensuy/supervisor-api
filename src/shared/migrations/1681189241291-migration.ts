45dd9bfd83c1fcd1f2091c0361faaa27602f71c4
Sensui <sensuijunior@hotmail.com>
2023-04-11 02:03:47 -0300

    feature: typeorm migration with one comand

--- src/shared/migrations/1681189241291-migration.ts
+++ src/shared/migrations/1681189241291-migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class init1678038513444 implements MigrationInterface {
-    name = 'init1678038513444'
+export class Migration1681189241291 implements MigrationInterface {
+    name = 'Migration1681189241291'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "name" text NOT NULL, "email" text NOT NULL, "password" text NOT NULL, "salt" text NOT NULL, "active" text NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
@@ -10,4 +10,5 @@ export class init1678038513444 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "user"`);
     }
+
 }
--- start.sh
+++ start.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-npm run migration:run
-
-npm start
+echo "Run/verifying migrations..."
+omit=$(npm run build)
+output_run=$(npm run migration:run)
+if [[ "$output_run" == *"No migrations are pending"* ]]; then
+    echo "No migrations are pending - Verifying if the database is up to date..."
+fi
+output_generate=$(npm run migration:generate src/shared/migrations/migration)
+if [[ "$output_generate" == *"No changes in database schema were found"* ]]; then
+    echo "No changes in database schema were found - No need generate a migration. Skipping..."
+else
+    echo "Generate and Running migrations..."
+    omit=$(npm run build)
+    npm run migration:run
+fi
+echo "Starting server..."
+npm run start:dev
